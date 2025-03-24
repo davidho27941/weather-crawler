b7da651e1e88fd14b4692a26870aecc420bd5efa
davidho27941 <davidho@gapp.nthu.edu.tw>
2025-03-24 15:33:21 +0800

    chore: add decorator-based logger.

---
+++ weather_crawler/logger.py
@@ -0,0 +1,43 @@
+import logging
+from logging import config
+
+
+def get_logger(name):
+
+    logging_config = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "loggers": {
+            "fastapi": {
+                "level": "INFO",
+                "handlers": ["console", "file"],
+                "propagate": False,
+            },
+            "uvicorn": {
+                "level": "INFO",
+                "handlers": ["console", "file"],
+                "propagate": False,
+            },
+            "drtoys_etl": {
+                "level": "INFO",
+                "handlers": ["console", "file"],
+                "propagate": False,
+            },
+        },
+        "handlers": {
+            "console": {"class": "logging.StreamHandler", "formatter": "detailed"},
+            "file": {
+                "class": "logging.FileHandler",
+                "formatter": "detailed",
+                "filename": "drtoys-etl.log",
+            },
+        },
+        "formatters": {
+            "detailed": {
+                "format": "%(asctime)s %(name)-15s %(levelname)-8s %(message)s"
+            }
+        },
+    }
+
+    config.dictConfig(logging_config)
+    return logging.getLogger(name)
