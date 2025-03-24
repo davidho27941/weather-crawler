1c9a1ed5ead6b1ea3cdbe6f2a9c1b0f85514336d
davidho27941 <davidho@gapp.nthu.edu.tw>
2025-03-24 16:52:21 +0800

    chore: add codex to fetch data from api.

--- weather_crawler/api.py
+++ weather_crawler/api.py
@@ -17,6 +17,7 @@ router = APIRouter(prefix="/v1")
 TOKEN = os.environ["CWA_AUTH_TOKEN"]
 
 CWA_WEATHER_URL = "https://opendata.cwa.gov.tw"
+AGRI_RAIN_FALL_URL = "https://data.moa.gov.tw"
 
 
 @request_with_restries
@@ -42,10 +43,30 @@ async def get_weather_data():
 
     url = f"{CWA_WEATHER_URL}/api/v1/rest/datastore/O-A0003-001"
 
+    status_code, responce_text = await get_data(url)
+
 
 @router.get("/weather_station", status_code=200)
-async def get_weather_station_info(): ...
+async def get_weather_station_info(type: str):
+
+    match type:
+        case "manned":
+            endpoint = "/api/v1/rest/datastore/C-B0074-001"
+        case "unmanned":
+            endpoint = "/api/v1/rest/datastore/O-B0074-002"
+        case _:
+            raise ValueError(f"Provided argument {type} is invalid.")
+
+    url = f"{CWA_WEATHER_URL}/{endpoint}"
+
+    status_code, responce_text = await get_data(url)
 
 
 @router.get("/rain_fall_station", status_code=200)
-async def get_rain_fall_station_data(): ...
+async def get_rain_fall_station_data():
+
+    endpoint = "/api/v1/TaiwanRainfallStationInformationType"
+
+    url = f"{AGRI_RAIN_FALL_URL}/{endpoint}"
+
+    status_code, responce_text = await get_data(url)
